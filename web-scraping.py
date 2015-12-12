1169ff6009d5c539f7a6eb3dde87a583d6e0f519
stg515 <stephentaras@yahoo.com>
2015-12-12 00:07:51 -0500

    updated scraping file to reflect changes in library

--- web-scraping.py
+++ web-scraping.py
@@ -1,12 +1,12 @@
-#Webscrape - Fabrics on Etsy updated 12/3, part 2
+#Webscrape - Fabrics on Etsy updated 12/11, part 3
 
 #Importing needed functions from our library#
 
 from ProjectLibrary import download_image
 from ProjectLibrary import build_dictionary
-from ProjectLibrary import Etsy_Website_Scraper
-from ProjectLibrary import Etsy_Images_Scraper
-import os
+from ProjectLibrary import Scraper
+from ProjectLibrary import Image_Name
+import os           #need this to make a folder to save the images in  
 import itertools    #need this to merge list of lists into one big list
 
 #next I will open a list to iterate through
@@ -20,11 +20,13 @@ website_file.close()
 scraped_sites_list = []     #empty list to hold a list of lists for all scraped sites
 scraped_images_list = []    #empty list to hold a list of lists for all scraped images
 
-for i in website_list:
-    scraped_sites_list.append(Etsy_Website_Scraper(i))
+for site in website_list:
+    temp_var = Scraper(site) #temporary variable to initiate Scraper class
+    scraped_sites_list.append(temp_var.scrapedSites()) #appends lists of scraped websites to website list
 
-for i in website_list:
-    scraped_images_list.append(Etsy_Images_Scraper(i))
+for site in website_list:
+    temp_var = Scraper(site) #temporary variable to initiate Scraper class
+    scraped_images_list.append(temp_var.scrapedImages()) #appends lists of scraped images to images list
 
 merged_websites = list(itertools.chain(*scraped_sites_list))    #this will unpack each list in the scraped sites list as one big list
 merged_images = list(itertools.chain(*scraped_images_list))     #this will unpack each list in the scraped images list as one big list
@@ -35,8 +37,11 @@ merged_images = list(itertools.chain(*scraped_images_list))     #this will unpac
 etsy_dictionary = build_dictionary(merged_websites,merged_images) #creates dictionary for direct purchase website & matching image
 
 create_folder = os.path.dirname(os.path.abspath(__file__)) #returns the path name to create a folder on a given computer
-destination = os.path.join(create_folder,'C:','Group14') #merges that path name with any other given directory information
-os.makedirs(destination) #tell python to create the specified folder based on the path name and given directory information above
+destination = os.path.join(create_folder,'Group14') #merges that path name with any other given directory information
+try:
+    os.makedirs(destination)            #this will try to create a folder 
+except OSError:
+    pass                                #however if you run it twice it will return an error, so this will ignore the error
 
 for i in merged_images:
     download_image(i)       #this function downloads and saves each image
